a5cabc29d11ad6a51947cf7ff26f57b5fd8f45c0
Nitesh Bagle <niteshbagle43@gmail.com>
2021-05-09 19:39:45 +0530

    http get method to pull movies

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+
+  async function fetchMovieHandler() {
+    const response = await fetch('https://swapi.dev/api/films/');
+    const data = await response.json();
+    setMovies(data.results);
+  };
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
